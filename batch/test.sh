ce701a5e9ca50b7524a316177c770913c3f6e8bf
Fajar J. Ekaputra <fajar.ekaputra@tuwien.ac.at>
2022-01-28 17:41:45 +0100

    batch tranwsformation

--- batch/test.sh
+++ batch/test.sh
@@ -4,12 +4,12 @@ for filename in ../src/test/resources/ttl/*.ttl;
 do
   basename $filename
   f="$(basename -- $filename)"
-  java -jar ../target/madmp-transformer-0.9-SNAPSHOT-jar-with-dependencies.jar -t ttl -i "$filename" -o "$f".json
+  java -jar madmp-transformer-0.9-SNAPSHOT-jar-with-dependencies.jar -t ttl -i "$filename" -o "$f".json
 done
 
 for filename in ../src/test/resources/json/*.json
 do
   basename $filename
   f="$(basename -- $filename)"
-  java -jar ../target/madmp-transformer-0.9-SNAPSHOT-jar-with-dependencies.jar -t json -i "$filename" -o "$f".ttl
+  java -jar madmp-transformer-0.9-SNAPSHOT-jar-with-dependencies.jar -t json -i "$filename" -o "$f".ttl
 done
